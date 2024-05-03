d208e8f08c381f1fdc654ff9c3d7cdf22a9704b6
Kenk <kenghin_lim@hotmail.com>
2024-05-04 00:01:16 +1000

    feat: add mock balances

---
+++ packages/app/src/mocks/balance.ts
@@ -0,0 +1,9 @@
+import type { TokenBalance } from "$types/types";
+
+export const MOCK_BALANCES: TokenBalance =
+{
+    "0x6982508145454ce325ddbe47a25d4ec3d2311933": "42069",
+    "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce": "42",
+    "0xdac17f958d2ee523a2206206994597c13d831ec7": "1",
+    "0x5e6b6d9abad9093fdc861ea1600eba1b355cd940": "0"
+}
