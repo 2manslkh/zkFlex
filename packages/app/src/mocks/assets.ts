89dad6e7a2ff025ccdf17efdb3d7bcf05f7f903d
Kenk <kenghin_lim@hotmail.com>
2024-05-04 00:01:34 +1000

    feat: add mock tokens

---
+++ packages/app/src/mocks/assets.ts
@@ -0,0 +1,79 @@
+import type { Token, TokenList } from "$types/types";
+
+export const MOCK_TOKENS: TokenList =
+{
+    "0x6982508145454ce325ddbe47a25d4ec3d2311933":
+    {
+        address: "0x6982508145454ce325ddbe47a25d4ec3d2311933",
+        name: "Pepe",
+        symbol: "PEPE",
+        decimals: 18,
+        logoURI: "https://assets.coingecko.com/coins/images/29850/standard/pepe-token.jpeg?1696528776",
+    },
+    "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce":
+    {
+        address: "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce",
+        name: "Shiba Inu",
+        symbol: "SHIB",
+        decimals: 18,
+        logoURI: "https://assets.coingecko.com/coins/images/11939/standard/shiba.png?1696511800",
+    },
+    "0xdac17f958d2ee523a2206206994597c13d831ec7":
+    {
+        address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+        name: "Tether USD",
+        symbol: "USDT",
+        decimals: 6,
+        logoURI: "https://assets.coingecko.com/coins/images/325/standard/Tether.png?1696501661",
+    },
+    "0x5e6b6d9abad9093fdc861ea1600eba1b355cd940":
+    {
+        address: "0x5e6b6d9abad9093fdc861ea1600eba1b355cd940",
+        name: "Dogecoin",
+        symbol: "DOGE",
+        decimals: 8,
+        logoURI: "https://assets.coingecko.com/coins/images/5/standard/dogecoin.png?1696501409",
+    },
+}
+
+
+
+
+
+// [
+
+//     // PEPE
+//     {
+//         address: "0x6982508145454ce325ddbe47a25d4ec3d2311933",
+//         name: "Pepe",
+//         symbol: "PEPE",
+//         decimals: 18,
+//         logoURI: "https://assets.coingecko.com/coins/images/29850/standard/pepe-token.jpeg?1696528776",
+//     },
+
+//     // SHIB
+//     {
+//         address: "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce",
+//         name: "Shiba Inu",
+//         symbol: "SHIB",
+//         decimals: 18,
+//         logoURI: "https://assets.coingecko.com/coins/images/11939/standard/shiba.png?1696511800",
+//     },
+//     // USDT
+//     {
+//         address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
+//         name: "Tether USD",
+//         symbol: "USDT",
+//         decimals: 6,
+//         logoURI: "https://assets.coingecko.com/coins/images/325/standard/Tether.png?1696501661",
+//     },
+//     // DOGE
+//     {
+//         address: "0x5e6b6d9abad9093fdc861ea1600eba1b355cd940",
+//         name: "Dogecoin",
+//         symbol: "DOGE",
+//         decimals: 8,
+//         logoURI: "https://assets.coingecko.com/coins/images/5/standard/dogecoin.png?1696501409",
+//     },
+
+// ]
