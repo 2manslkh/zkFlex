a87f409e4a3ce1247dacba560f410e896b970e2e
Kenk <kenghin_lim@hotmail.com>
2024-05-05 01:02:22 +1000

    try

--- packages/app/src/routes/api/+server.ts
+++
@@ -1,7 +0,0 @@
-
-import { error, json } from '@sveltejs/kit';
-
-/** @type {import('./$types').RequestHandler} */
-export async function GET({ url }) {
-    return json({ message: 'Hello world' })
-}
