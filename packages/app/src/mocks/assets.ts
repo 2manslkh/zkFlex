598146b248ee08255388f76bae9ec87df5e592d0
Kenk <kenghin_lim@hotmail.com>
2024-05-05 05:27:41 +1000

    update assets for each chain

--- packages/app/src/mocks/assets.ts
+++ packages/app/src/mocks/assets.ts
@@ -5,35 +5,35 @@ export const MOCK_TOKENS: TokenList =
     "0x6982508145454ce325ddbe47a25d4ec3d2311933":
     {
         address: "0x6982508145454ce325ddbe47a25d4ec3d2311933",
-        name: "Pepe",
+        name: "Pepe (Ethereum)",
         symbol: "PEPE",
         decimals: 18,
         logoURI: "https://assets.coingecko.com/coins/images/29850/standard/pepe-token.jpeg?1696528776",
     },
-    "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce":
+    "0x4515A45337F461A11Ff0FE8aBF3c606AE5dC00c9":
     {
-        address: "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce",
-        name: "Shiba Inu",
-        symbol: "SHIB",
+        address: "0x4515A45337F461A11Ff0FE8aBF3c606AE5dC00c9",
+        name: "Merchant Moe (Mantle)",
+        symbol: "MOE",
         decimals: 18,
-        logoURI: "https://assets.coingecko.com/coins/images/11939/standard/shiba.png?1696511800",
+        logoURI: "https://assets.coingecko.com/coins/images/34422/standard/MOE.png?1704821927",
     },
-    // "0xdac17f958d2ee523a2206206994597c13d831ec7":
-    // {
-    //     address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
-    //     name: "Tether USD",
-    //     symbol: "USDT",
-    //     decimals: 6,
-    //     logoURI: "https://assets.coingecko.com/coins/images/325/standard/Tether.png?1696501661",
-    // },
-    // "0x5e6b6d9abad9093fdc861ea1600eba1b355cd940":
-    // {
-    //     address: "0x5e6b6d9abad9093fdc861ea1600eba1b355cd940",
-    //     name: "Dogecoin",
-    //     symbol: "DOGE",
-    //     decimals: 8,
-    //     logoURI: "https://assets.coingecko.com/coins/images/5/standard/dogecoin.png?1696501409",
-    // },
+    "0x0bd4887f7d41b35cd75dff9ffee2856106f86670":
+    {
+        address: "0x0bd4887f7d41b35cd75dff9ffee2856106f86670",
+        name: "Friend.tech (Base)",
+        symbol: "FRIEND",
+        decimals: 18,
+        logoURI: "https://assets.coingecko.com/coins/images/37475/standard/friend.jpeg?1714464901",
+    },
+    "0x3845badade8e6dff049820680d1f14bd3903a5d0":
+    {
+        address: "0x3845badade8e6dff049820680d1f14bd3903a5d0",
+        name: "The Sandbox (Polygon)",
+        symbol: "SAND",
+        decimals: 18,
+        logoURI: "https://assets.coingecko.com/coins/images/12129/standard/sandbox_logo.jpg?1696511971",
+    },
 }
 
 
--- packages/app/src/routes/proof/[slug]/+page.svelte
+++ packages/app/src/routes/proof/[slug]/+page.svelte
@@ -9,22 +9,6 @@
   export let data: PageServerData;
 </script>
 
-<svelte:head>
-  <meta property="og:title" content="zkFlex" />
-  <meta property="og:description" content="My size is size." />
-  <meta property="og:image" content="https://zkflex.lol/noun/a" />
-  <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="600" />
-  <meta property="og:url" content="https://zkflex.lol" />
-  <meta property="og:type" content="website" />
-
-  <meta name="twitter:card" content="summary_large_image" />
-  <meta name="twitter:title" content="zkFlex" />
-  <meta name="twitter:description" content="My size is size." />
-  <meta name="twitter:image" content="https://zkflex.lol/noun/a" />
-</svelte:head>
-
-<MetaTags title="zkFlex | Proof" />
 <!-- Body -->
 <Page>
   <div class="f-center flex-col gap-2 lg:w-[530px]">
