61f819960691ad1d8e640be0b17769355ecf4d7b
Kenk <kenghin_lim@hotmail.com>
2024-05-05 06:51:07 +1000

    feat: Update user rank calculation thresholds
    The code changes modify the `calculateUserRank` function in the `calculateUserStatus.ts` file. The thresholds for determining the user's rank based on their asset balance have been adjusted.
    
    - If the asset balance is greater than 100,000, the user is considered a "whale".
    - If the asset balance is greater than 1,000, the user is considered a "shark".
    - If the asset balance is greater than 10, the user is considered a "dolphin".
    - If the asset balance is 0 or less, the user is considered a "shrimp".
    - Otherwise, the user is considered a "ghost".
    
    This update improves the accuracy of the user rank calculation.

--- packages/app/src/libs/util/calculateUserStatus.ts
+++ packages/app/src/libs/util/calculateUserStatus.ts
@@ -3,14 +3,14 @@ import type { FlexType } from "../../types/types";
 
 export function calculateUserRank(assetBalance: string): FlexType {
     let holderStatus: FlexType;
-    if (parseEther(assetBalance) > parseEther('1000')) {
+    if (parseEther(assetBalance) > parseEther('100000')) {
         holderStatus = 'whale';
-    } else if (parseEther(assetBalance) > parseEther('40')) {
+    } else if (parseEther(assetBalance) > parseEther('1000')) {
         holderStatus = 'shark';
-    } else if (parseEther(assetBalance) > parseEther('0')) {
+    } else if (parseEther(assetBalance) > parseEther('10')) {
         holderStatus = 'dolphin';
     } else if (parseEther(assetBalance) > parseEther('0')) {
-        holderStatus = 'dolphin';
+        holderStatus = 'shrimp';
     } else {
         holderStatus = 'ghost';
     }
