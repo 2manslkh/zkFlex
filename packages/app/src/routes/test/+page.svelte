91c654bdea0e267468bfa4becbc6fcc801f0eb0d
Kenk <kenghin_lim@hotmail.com>
2024-05-04 11:12:57 +1000

    try again?

--- packages/app/src/routes/test/+page.svelte
+++ packages/app/src/routes/test/+page.svelte
@@ -21,14 +21,13 @@
 
 <svelte:head>
   <title>zkFlex - Gallery</title>
-  <meta property="og:title" content="Simple Preview" />
-  <meta property="og:description" content="This is a simple preview example." />
-  <meta property="og:image" content="https://via.placeholder.com/1200x630" />
-  <meta property="og:image:width" content="1200" />
-  <meta property="og:image:height" content="630" />
-  <meta property="og:url" content="https://example.com" />
-  <meta property="og:type" content="website" />
-
+  <meta property="og:title" content="The Rock" />
+  <meta property="og:image" content="https://example.com/ogp.jpg" />
+  <meta property="og:image:secure_url" content="https://secure.example.com/ogp.jpg" />
+  <meta property="og:image:type" content="image/jpeg" />
+  <meta property="og:image:width" content="400" />
+  <meta property="og:image:height" content="300" />
+  <meta property="og:image:alt" content="A shiny red apple with a bite taken out" />
   <!-- Twitter meta tags -->
   <meta name="twitter:card" content="summary_large_image" />
   <meta name="twitter:title" content="Simple Preview" />
