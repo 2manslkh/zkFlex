48b2582cb3a6b56963815f583b79a73dbc79a683
Kenk <kenghin_lim@hotmail.com>
2024-05-05 07:11:19 +1000

    bugfix

--- packages/app/src/routes/proof/[slug]/+page.svelte
+++ packages/app/src/routes/proof/[slug]/+page.svelte
@@ -15,19 +15,19 @@
   <meta name="description" content="My size is size." />
 
   <!-- Facebook Meta Tags -->
-  <meta property="og:url" content="https://zkflex.lol/proof/${$page.params.slug}" />
+  <meta property="og:url" content="https://zkflex.lol/proof/{$page.params.slug}" />
   <meta property="og:type" content="website" />
   <meta property="og:title" content="zkFlex | Proof" />
   <meta property="og:description" content="My size is size." />
-  <meta property="og:image" content="https://zkflex.lol/noun/render/${$page.params.slug}" />
+  <meta property="og:image" content="https://zkflex.lol/noun/render/{$page.params.slug}" />
 
   <!-- Twitter Meta Tags -->
   <meta name="twitter:card" content="summary_large_image" />
   <meta property="twitter:domain" content="zkflex.lol" />
-  <meta property="twitter:url" content="https://zkflex.lol/proof/${$page.params.slug}" />
+  <meta property="twitter:url" content="https://zkflex.lol/proof/{$page.params.slug}" />
   <meta name="twitter:title" content="zkFlex | Proof" />
   <meta name="twitter:description" content="My size is size." />
-  <meta name="twitter:image" content="https://zkflex.lol/noun/render/${$page.params.slug}" />
+  <meta name="twitter:image" content="https://zkflex.lol/noun/render/{$page.params.slug}" />
 </svelte:head>
 
 <!-- Body -->
