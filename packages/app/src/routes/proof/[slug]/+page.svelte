a72c0c1c158a52ff8f2c02cf581b2a5b248d1d9e
Kenk <kenghin_lim@hotmail.com>
2024-05-04 18:02:20 +1000

    update

--- packages/app/src/routes/proof/[slug]/+page.svelte
+++ packages/app/src/routes/proof/[slug]/+page.svelte
@@ -2,24 +2,59 @@
   import { Page } from '$components/Page';
   import type { PageServerData } from './$types';
   import { generateNounsSVG } from '$libs/nouns';
-  import pepe from '$images/pepe.svg';
   import { onMount } from 'svelte';
   import { MetaTags } from 'svelte-meta-tags';
   import { draftMessage } from '$libs/twitter';
   import { page } from '$app/stores';
   import FlexCard from '$components/FlexCard/FlexCard.svelte';
+  import { supabaseClient } from '$libs/supabase';
 
   let svgBase64 = '';
   let bg = '';
+
+  // Function to save SVG to Supabase storage
+  async function saveSvgToSupabase(filename: string, svgData: string) {
+    try {
+      // Upload SVG to Supabase storage
+      const { data, error } = await supabaseClient.storage.from('ethsydney').upload(`${filename}.svg`, svgData);
+
+      if (error) {
+        console.error('Error uploading SVG to Supabase:', error.message);
+      } else {
+        console.log('SVG uploaded successfully');
+      }
+    } catch (error) {
+      console.error('Error:');
+    }
+  }
 
   function handleGenerate(hash: string) {
     let { svg, background } = generateNounsSVG('0x' + hash);
     svgBase64 = svg;
     bg = background;
   }
+
+  async function saveSvg() {
+    console.log('asd');
+    const svgElement = document.getElementById(data.hash);
+    console.log('🚀 | saveSvg | svgElement:', svgElement);
+
+    if (!svgElement) {
+      return;
+    }
+
+    // Convert SVG to a data URL
+    const svgXml = new XMLSerializer().serializeToString(svgElement);
+    console.log('🚀 | saveSvg | svgXml:', svgXml);
+    await saveSvgToSupabase(data.hash, svgXml);
+  }
   export let data: PageServerData;
   onMount(() => {
-    handleGenerate(data.hash);
+    // add timeout to ensure the svg is rendered
+    setTimeout(() => {}, 5000);
+    saveSvg().then(() => {
+      console.log('svg saved');
+    });
   });
 </script>
 
@@ -30,10 +65,8 @@
     <div class="display-large-medium">ZK CERTIFIED ✅</div>
     <!-- <div class="display-medium">{data.tokenAddress}</div> -->
     <!-- <div class="body-bold">{data.proof}</div> -->
-    {#if svgBase64}
-      <FlexCard
-        card={{ twitterName: data.twitterName, rank: data.rank, tokenName: data.tokenAddress, proofHash: data.hash }} />
-    {/if}
+    <FlexCard
+      card={{ twitterName: data.twitterName, rank: data.rank, tokenName: data.tokenAddress, proofHash: data.hash }} />
 
     <a href={draftMessage($page.url.toString())}>SHARE</a>
   </div>
