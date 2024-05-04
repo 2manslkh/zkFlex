34c5dc0883865369617900798671c9a214b19eb6
ben <bennettlikebeckham@gmail.com>
2024-05-04 09:49:58 +0800

    fix: tags

--- packages/app/src/routes/proof/[slug]/+page.svelte
+++ packages/app/src/routes/proof/[slug]/+page.svelte
@@ -7,8 +7,19 @@
 
 <svelte:head>
   <!-- Additional head tags -->
+  <meta property="og:type" content="en_US" />
+  <meta property="website" content="website" />
+  <meta property="og:title" content="ZK Flex" />
+  <meta property="og:description" content="Flex your size" />
+  <meta property="og:url" content="https://zk-flex.vercel.app" />
+  <meta property="og:site_name" content="ZK Flex" />
   <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
-  <meta name="twitter:card" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:creator" content="@ZKFlex" />
+  <meta name="twitter:url" content="https://twitter.com/zkflex" />
+  <meta name="twitter:title" content="ZK Flex" />
+  <meta name="twitter:description" content="Flex your size" />
+  <meta charset="utf-8" />
   <!-- Additional head tags -->
 </svelte:head>
 <!-- Body -->
